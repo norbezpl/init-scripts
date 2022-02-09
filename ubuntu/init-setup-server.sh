e3dfeab0a9251df6280d022943226c365bd863ed
norbezpl <norbertrom01@gmail.com>
2022-02-09 17:36:21 +0100

    Server changes

--- ubuntu/init-setup-server.sh
+++ ubuntu/init-setup-server.sh
@@ -6,7 +6,7 @@
 
 apt update
 apt upgrade -y
-apt install net-tools htop ssh gdebi mc
+apt install -y net-tools htop ssh gdebi mc libgee-0.8-dev libjson-glib-dev libvte-2.91-dev valac aria2 lsb-release aptitude
 
 # Konfiguracja git
 read -p "Wpisz adres e-mail, aby skonfigurować zatwierdzenia git (ten sam, który jest używany na Twoim koncie): " git_email
